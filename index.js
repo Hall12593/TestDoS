3f3315cf778bb9ddbf1fd4e650ab463637d682e0
Hall127 <witherboss127@proton.me>
2025-07-17 19:56:05 -0600

    Update index.js
    Translating the spanish comments to english

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const axios = require("axios");
 const { Worker, isMainThread, parentPort } = require("worker_threads");
 
-const TARGET_URL = "http://localhost:10617"; // La URL a atacar
-const REQUESTS_PER_WORKER = 1000; // La cantidad de peticiones que hara cada worker
+const TARGET_URL = "http://localhost:10617"; // The Victim URL
+const REQUESTS_PER_WORKER = 1000; // How many requests per worker
 
 async function sendRequests() {
   for (let i = 0; i < REQUESTS_PER_WORKER; i++) {
@@ -17,7 +17,7 @@ async function sendRequests() {
 }
 
 if (isMainThread) {
-  const WORKER_COUNT = 10; // Número de hilos paralelos
+  const WORKER_COUNT = 10; // Number of parallel threads
 
   for (let i = 0; i < WORKER_COUNT; i++) {
     const worker = new Worker(__filename);
